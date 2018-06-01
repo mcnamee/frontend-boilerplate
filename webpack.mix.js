eda3973de3a2eab2586c112b272e91fca5bd9929
Matt Mcnamee <matt@mcnamee.co>
2018-06-01 16:42:59 +1000

    Switch to browser-sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,9 @@
 const mix = require('laravel-mix');
-const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 mix
   .js('src/assets/js/app.js', 'dist')
   .sass('src/assets/scss/app.scss', 'dist')
   .copy('src/index.html', 'dist')
   .copyDirectory('src/assets/images', 'dist/images')
-  .webpackConfig({ plugins: [new LiveReloadPlugin()] })
+  .browserSync('frontend-boilerplate.localhost')
   .setPublicPath('dist');
