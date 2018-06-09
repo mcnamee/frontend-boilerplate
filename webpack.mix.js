df7497f58ac4570ab59add01f3940fb650ee12db
Matt Mcnamee <matt@mcnamee.co>
2018-06-10 09:21:36 +1000

    Fix for image error

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,7 @@ mix
   .js('src/assets/js/app.js', 'dist/js')
   .extract(['bootstrap', 'jquery', 'popper.js'])
   .sass('src/assets/scss/app.scss', 'dist/css')
+  .options({ processCssUrls: false })
   .copy('src/index.html', 'dist')
   .copyDirectory('src/assets/images', 'dist/images')
   .browserSync('frontend-boilerplate.localhost')
